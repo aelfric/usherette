fab110d60f59502d5575c186807e9dd6759056df
Frank Riccobono <F.Riccobono@gmail.com>
2013-04-03 23:14:34 -0400

    added rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usherette::Application.config.secret_token = 'cc77a56317614f4ef7cc8f48983446bccb62ed6f390f3a7aeee934a785919148e45c260b8baf807a5ad62d4135acafa54ee3160ab15db67471fd8feb0f6e5ad3'
