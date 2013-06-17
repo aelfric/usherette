fcf1720bead76b1173371f10c5b1cd9a6d853755
Frank Riccobono <F.Riccobono@gmail.com>
2013-06-16 23:35:48 -0400

    working on look and feel

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,7 @@
 class Show < ActiveRecord::Base
     has_many :performances
   attr_accessible :description, :title
+  def description_to_html 
+      return self.description.gsub("\n", "<br />").html_safe
+  end
 end
