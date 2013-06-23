f5ebbd38551b62877ef8d66ac83f417f57b015b9
Frank Riccobono <F.Riccobono@gmail.com>
2013-06-22 22:47:59 -0400

    use config variable to connect to paypal

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,7 +10,7 @@ class CartsController < ApplicationController
         @cart = current_cart
         if @cart.update_attributes(params[:cart].merge(:placed_at => Time.now()))
             @cart.save
-            redirect_to "https://www.sandbox.paypal.com/cgi-bin/webscr?"+ {"cmd" => "_s-xclick", "encrypted" => @current_cart.paypal_encrypted_url(performances_url, payment_notifications_url)}.to_query
+            redirect_to APP_CONFIG[:paypal_url]+ "?"+ {"cmd" => "_s-xclick", "encrypted" => @current_cart.paypal_encrypted_url(performances_url, payment_notifications_url)}.to_query
         else
             render :edit
         end
