a4da2574fb2abd0e66275ad0f44967fb16514eba
Frank Riccobono <F.Riccobono@gmail.com>
2013-11-03 11:08:40 -0500

    restrict login page to SSL

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Usherette
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #rack-ssl-enforcer
+    config.middleware.use Rack::SslEnforcer, :only => %r{^/users/}
   end
 end
