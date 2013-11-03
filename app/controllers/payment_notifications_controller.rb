794dd92e66046a5c54de540b08fb42341b0acbcd
Frank Riccobono <F.Riccobono@gmail.com>
2013-11-03 09:59:00 -0500

    beginnings of the paypal report

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,10 +1,15 @@
 class PaymentNotificationsController < ApplicationController
     protect_from_forgery :except => [:create]
+    before_filter :authenticate_user!, :only => :index
     def create
         PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
         @cart = Cart.find(params[:invoice])
         @cart.order_email = params[:payer_email]
         @cart.save
         render :nothing => true
+    end
+
+    def index
+        @payment_notifications = PaymentNotification.all
     end
 end
