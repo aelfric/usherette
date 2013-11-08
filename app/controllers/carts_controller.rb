cad3aba7f6f9cbed4c0827081405a229f7108abd
Frank Riccobono <F.Riccobono@gmail.com>
2013-11-07 20:48:57 -0500

    use POST to hit paypal rather than GET to avoid IE long URL restriction

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -32,7 +32,7 @@ class CartsController < ApplicationController
         @cart = current_cart
         if @cart.update_attributes(params[:cart].merge(:placed_at => Time.now()))
             @cart.save
-            redirect_to APP_CONFIG[:paypal_url]+ "?"+ {"cmd" => "_s-xclick", "encrypted" => @current_cart.paypal_encrypted_url(performances_url, payment_notifications_url)}.to_query
+            @encrypted = @current_cart.paypal_encrypted_url(performances_url, payment_notifications_url)
         else
             render :edit
         end
