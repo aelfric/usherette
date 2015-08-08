80863e63e40a8cd5a3abd2d8f3ad19029044b5b0
Frank Riccobono <F.Riccobono@gmail.com>
2015-08-08 13:23:44 -0400

    error guard on IPNs not related to cart transactions

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -2,11 +2,13 @@ class PaymentNotificationsController < ApplicationController
     protect_from_forgery :except => [:create]
     before_filter :authenticate_user!, :only => :index
     def create
-        PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
-        @cart = Cart.find(params[:invoice])
-        @cart.order_email = params[:payer_email]
-        @cart.save
-        render :nothing => true
+        if params[:txn_type] == 'cart'
+            PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
+            @cart = Cart.find(params[:invoice])
+            @cart.order_email = params[:payer_email]
+            @cart.save
+            render :nothing => true
+        end
     end
 
     def index
